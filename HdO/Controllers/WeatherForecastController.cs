180c93d6548f02635bf9b18f6d217c35c6e8689a
José Vinícius Henrique Pereira <jose.vinicius@t2s.com.br>
2023-08-12 13:00:05 -0300

    feat: Altera mensagem redis;

--- HdO/Controllers/WeatherForecastController.cs
+++ HdO/Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@ namespace HdO.Controllers
 
             ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(config);
             IDatabase db = redis.GetDatabase();
-            db.StringSet("foo", "Testado");
+            db.StringSet("foo", "Testando pipeline");
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
